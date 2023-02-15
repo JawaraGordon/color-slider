0a5529238c1c1c5c96d45a14c6f45ff50b21f6ef
JawaraGordon <jawaragordon@icloud.com>
2023-02-14 20:48:53 -0500

    created a brute force solution for updating BG with setInterval

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const r = document.querySelector('#r');
 const g = document.querySelector('#g');
 const b = document.querySelector('#b');
 
-const rOut = document.querySelector('#r-out');
-const gOut = document.querySelector('#g-out');
-const bOut = document.querySelector('#b-out');
+let rOut = document.querySelector('#r-out');
+let gOut = document.querySelector('#g-out');
+let bOut = document.querySelector('#b-out');
+
+const body = document.body;
 
 console.log('r', r.value);
 console.log('g', g.value);
@@ -16,11 +18,29 @@ console.log('b', bOut.value);
 
 r.addEventListener('change', () => {
   console.log('r', r.value);
-  rOut.value = r.value;
+  rOut = parseInt(r.value);
+  console.log('inside', typeof rOut);
+  return rOut;
 });
+
 g.addEventListener('change', () => {
   console.log('r', g.value);
+  gOut = parseInt(g.value);
+  console.log('inside', typeof gOut);
+  return gOut;
 });
 b.addEventListener('change', () => {
   console.log('r', b.value);
+  bOut = parseInt(b.value);
+  console.log('inside', typeof bOut);
+  return bOut;
 });
+
+console.log('body', body);
+
+setInterval(() => {
+  console.log('rOut', rOut);
+  console.log('gOut', gOut);
+  console.log('bOut', bOut);
+  body.style.backgroundColor = 'rgb(' + rOut + ', ' + gOut + ', ' + bOut + ')';
+}, 100);
