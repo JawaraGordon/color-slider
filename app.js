61e124f54e61c427c8fcfb3e59d42a4d5c619325
JawaraGordon <jawaragordon@icloud.com>
2023-02-14 19:02:19 -0500

    added out values to fieldsets, hex value block, styling to form, declared rbg variables in app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+const r = document.getElementById('#r')
+const g = document.getElementById('#g')
+const b = document.getElementById('#b')
+
+const rOut = document.getElementById('#r-out')
+const gOut = document.getElementById('#g-out')
+const bOut = document.getElementById('#b-out')
+
