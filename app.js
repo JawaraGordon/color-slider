8bd7d7ed77ddbe53838c2af917cba4bd4b328222
JawaraGordon <jawaragordon@icloud.com>
2023-02-14 19:15:22 -0500

    testing for r.value changes with eventListener

--- app.js
+++ app.js
@@ -6,4 +6,14 @@ const rOut = document.querySelector('#r-out');
 const gOut = document.querySelector('#g-out');
 const bOut = document.querySelector('#b-out');
 
-console.log('r', r);
+console.log('r', r.value);
+console.log('g', g.value);
+console.log('b', b.value);
+
+console.log('r', rOut.value);
+console.log('g', gOut.value);
+console.log('b', bOut.value);
+
+r.addEventListener('change', () => {
+  console.log('r', r.value);
+});
