ec4f8dff1849e0e645dc117241c8f6b4656e0053
JawaraGordon <jawaragordon@icloud.com>
2023-02-16 05:55:03 -0500

    created function to change hex output to equal rgb values toString(16)

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const rOut = document.querySelector('#r-out');
 const gOut = document.querySelector('#g-out');
 const bOut = document.querySelector('#b-out');
 
+const hexOut = document.querySelector('#hex');
+
 const body = document.body;
 
 console.log('r', r.value);
@@ -19,22 +21,41 @@ console.log('b', bOut.value);
 r.addEventListener('change', () => {
   rOut.value = r.value;
   rOutputNum = parseInt(r.value);
+  setHexValue();
   setRbgColor();
+  return rOutputNum;
 });
 
 g.addEventListener('change', () => {
   gOut.value = g.value;
   gOutputNum = parseInt(g.value);
+  setHexValue();
   setRbgColor();
+  return gOutputNum;
 });
 
 b.addEventListener('change', () => {
   bOut.value = b.value;
   bOutputNum = parseInt(b.value);
+  setHexValue();
   setRbgColor();
+  return bOutputNum;
 });
 
 const setRbgColor = () => {
   body.style.backgroundColor =
     'rgb(' + rOutputNum + ', ' + gOutputNum + ', ' + bOutputNum + ')';
+};
+
+const setHexValue = () => {
+  rHex = rOutputNum.toString(16);
+  gHex = gOutputNum.toString(16);
+  bHex = bOutputNum.toString(16);
+  console.log(rHex);
+  console.log(gHex);
+  console.log(bHex);
+  hex = '#' + rHex + gHex + bHex;
+  console.log(hex);
+  hexOut.value = hex;
+  console.log(hex.value);
 };
